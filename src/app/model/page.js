582ceceb1df7912e9990863768577e0b589ec3ee
James Akpan <jamesakpan016@gmail.com>
2024-04-27 19:45:28 +0100

    Adding model page

--- src/app/model/page.js
+++ src/app/model/page.js
@@ -1,20 +1,32 @@
 "use client";
 import { PageContainer } from "@/Components/Global/PageContainer";
 import { Models } from "@/utils/modelJSON";
-import { Flex, Heading, Image, VStack } from "@chakra-ui/react";
+import { Box, Flex, Heading, Image, VStack } from "@chakra-ui/react";
+import { IoMdCloseCircleOutline } from "react-icons/io";
 
 const Model = () => {
   const model = Models[0];
 
   return (
     <PageContainer>
-      <Flex>
-        <VStack align="start">
-          <Image alt={`${model.name} on Balaclava Girls`} src="cvcTest.png" />
-          <Heading color="white" fontFamily="Clashmid">
-            {model.name}
-          </Heading>
-        </VStack>
+      <Flex w="100%">
+        <Flex align="center" w="50%" m="0 auto" flexDir="column" mt={12}>
+          <VStack align="start" h="80vh" borderBottom="1px solid red">
+            <Box
+              color="white"
+              fontFamily="Clashbold"
+              p="2"
+              bg="primary"
+              w="100%"
+            >
+              <IoMdCloseCircleOutline fontSize="30px" cursor="pointer" />
+            </Box>
+            <Image alt={`${model.name} on Balaclava Girls`} src="cvcTest.png" />
+            <Heading color="white" fontFamily="Clashmid">
+              {model.name}
+            </Heading>
+          </VStack>
+        </Flex>
       </Flex>
     </PageContainer>
   );
