2e136808fc33f29df05316bb246166648ee73a8d
James Akpan <jamesakpan016@gmail.com>
2024-05-17 04:24:49 +0100

    Fav Ico

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,16 @@ import ThemeProvider from "./theme-provider";
 export default function RootLayout({ children }) {
   return (
     <html>
+      {/* Insert site icon below */}
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          href="/favicon.ico"
+          type="image/x-icon"
+          sizes="16x16"
+        ></link>
+      </head>
       <body style={{ backgroundColor: "black" }}>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
