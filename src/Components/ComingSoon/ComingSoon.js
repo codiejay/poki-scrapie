d9610988320783533dca860574c299bc67e36a1b
James Akpan <jamesakpan016@gmail.com>
2024-05-18 14:29:56 +0100

    Add video

--- src/Components/ComingSoon/ComingSoon.js
+++ src/Components/ComingSoon/ComingSoon.js
@@ -33,13 +33,17 @@ export const ComingSoon = () => {
           }}
           alignContent="center"
           justifyItems="center"
-          w="100%"
+          justifyContent="center"
+          w={{
+            base: "100%",
+            "2xl": "50%",
+          }}
           gap={{
             base: "20px",
             lg: "30px",
           }}
+          m="0 auto"
         >
-          {/* First SlideFade Component */}
           <SlideFade
             in={true}
             offsetY={{ base: "50px", lg: "190px" }}
@@ -56,7 +60,36 @@ export const ComingSoon = () => {
               }}
               bg="#0F2940"
               borderRadius="12px"
-            ></Flex>
+              pos="relative"
+            >
+              <Box
+                pos="absolute"
+                w="100%"
+                h="100%"
+                top="0"
+                left="0"
+                bg="#0F2940"
+                zIndex="2"
+                opacity="0.8"
+                borderRadius="12px"
+              />
+              <video
+                style={{
+                  width: "100%", // Ensures the video covers the full width of the Flex container
+                  height: "100%", // Ensures the video covers the full height of the Flex container
+                  objectFit: "cover", // Ensures the video covers the area without distorting aspect ratio
+                  position: "absolute", // Positions video correctly within the Flex container
+                  top: 0,
+                  left: 0,
+                  borderRadius: "12px",
+                  zIndex: "1", // Rounds the corners of the video
+                }}
+                autoPlay
+                loop
+                muted
+                src="/coffeedrop.mp4" // Adjust the path if your video is located elsewhere
+              />
+            </Flex>
           </SlideFade>
 
           {/* Second SlideFade Component */}
@@ -98,7 +131,36 @@ export const ComingSoon = () => {
               }}
               bg="#0F2940"
               borderRadius="12px"
-            ></Flex>
+              pos="relative"
+            >
+              <Box
+                pos="absolute"
+                w="100%"
+                h="100%"
+                top="0"
+                left="0"
+                bg="#0F2940"
+                zIndex="2"
+                opacity="0.8"
+                borderRadius="12px"
+              />
+              <video
+                style={{
+                  width: "100%", // Ensures the video covers the full width of the Flex container
+                  height: "100%", // Ensures the video covers the full height of the Flex container
+                  objectFit: "center", // Ensures the video covers the area without distorting aspect ratio
+                  position: "absolute", // Positions video correctly within the Flex container
+                  top: 0,
+                  left: 0,
+                  borderRadius: "12px",
+                  zIndex: "1", // Rounds the corners of the video
+                }}
+                autoPlay
+                loop
+                muted
+                src="/chocolateHaze.mp4" // Adjust the path if your video is located elsewhere
+              />
+            </Flex>
           </SlideFade>
         </Grid>
       </Flex>
@@ -115,7 +177,7 @@ export const ComingSoon = () => {
         <HStack
           fontSize={{
             base: "12px",
-            lg: "16px",
+            lg: "14px",
           }}
           gap="2px"
           mt="2"
