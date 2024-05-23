7cc075c6e04d3a208dd6754c9b6edd1fe1483ebc
James Akpan <jamesakpan016@gmail.com>
2024-05-23 17:46:54 +0100

    Update component name from small letter to capital

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,14 @@
 "use client";
 import { Box, Flex } from "@chakra-ui/react";
-import { About } from "../components/About/About";
-import { Acts } from "../components/Acts/Acts";
-import { FiguresQuotes } from "../components/Figures";
-import { Footer } from "../components/Footer/Footer";
-import { FooterInfo } from "../components/Footer/FooterInfo";
-import { Header } from "../components/Header/Header";
-import { JoinSection } from "../components/JoinSection/JoinSection";
-import { Showcase } from "../components/Showcase/Showcase";
-import { Team } from "../components/Team/Team";
+import { About } from "../Components/About/About";
+import { Acts } from "../Components/Acts/Acts";
+import { FiguresQuotes } from "../Components/Figures";
+import { Footer } from "../Components/Footer/Footer";
+import { FooterInfo } from "../Components/Footer/FooterInfo";
+import { Header } from "../Components/Header/Header";
+import { JoinSection } from "../Components/JoinSection/JoinSection";
+import { Showcase } from "../Components/Showcase/Showcase";
+import { Team } from "../Components/Team/Team";
 
 const LimitLayout = ({ children }) => {
   return (
